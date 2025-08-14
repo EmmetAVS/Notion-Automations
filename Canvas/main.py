12e7d31bbd29ed760f9f7cd4b5fb40db56bc1696
Aarush <aarush.shrikanth@gmail.com>
2025-08-13 21:31:30 -0700

    Canvas --> Notion automation

---
+++ Canvas/main.py
@@ -0,0 +1,397 @@
+from email import header
+from wsgiref import headers
+import requests as r
+import copy
+import json
+from datetime import datetime as dt, timezone
+from bs4 import BeautifulSoup as bs
+
+integration_title = "Canvas"
+
+config_defaults = {
+    "canvases": [
+        
+    ]
+}
+
+canvas_defaults = {
+    "canvas-api-url": None,
+    "canvas-api-token": None,
+    "excluded-course-codes": [],
+}
+
+expected_title = "School Tasks"
+
+database_format = {
+    "properties": {
+        "Name": {
+            "title": {}
+        },
+        "Due Date": {
+            "date": {}
+        },
+        "Description": {
+            "rich_text": {}
+        },
+        "Status": {
+            "select": {
+                "options": [
+                    {"name": "Not started", "color": "red"},
+                    {"name": "In progress", "color": "yellow"},
+                    {"name": "Submitted", "color": "blue"},
+                    {"name": "Graded", "color": "green"}
+                ]
+            }
+        },
+        "Course": {
+            "select": {
+                "options": [
+                ]
+            }
+        },
+        "Canvas-Assignment-ID": {
+            "rich_text": {}
+        },
+        "Link": {
+            "url": {}
+        }
+    }
+}
+
+def get_assignments(course, api_key):
+    
+    response = r.get(f"{course['url']}/assignments", headers={
+        "Authorization": f"Bearer {api_key}",
+    })
+    
+    response.raise_for_status()
+    
+    assignments = []
+
+    for assignment in response.json():
+        if not assignment.get('due_at'):
+            continue
+        
+        assignments.append({
+            "id": str(assignment['id']),
+            "name": assignment['name'],
+            "due_at": assignment['due_at'],
+            "description": assignment['description'],
+            "submitted": assignment.get('has_submitted_submissions', False),
+            "graded": assignment.get('graded_submissions_exist', False),
+            "course": course['name'],
+            "url": assignment.get('html_url', "")
+        })
+
+    return assignments
+
+def confirm_notion_database(data, assignments):
+    headers = {
+        "Authorization": f"Bearer {data.get('Notion').get('Notion-API-Key')}",
+        'Content-Type': 'application/json',
+        'Notion-Version': '2022-06-28'
+    }
+    
+    parent = {
+        "type": "page_id",
+        "page_id": data['Notion']["parent-page-id"]
+    }
+    
+    search_request = r.post("https://api.notion.com/v1/search", headers=headers, json={
+        "query": expected_title,
+        "filter": {
+            "value": "database",
+            "property": "object"
+        },
+    })
+    
+    id = None
+    
+    now = dt.now(timezone.utc)
+    
+    new_assignments = [
+        assignment for assignment in assignments if dt.fromisoformat(assignment['due_at'].replace("Z", "+00:00")) >= now
+    ]
+
+    other_assignments = [
+        assignment for assignment in assignments if dt.fromisoformat(assignment['due_at'].replace("Z", "+00:00")) < now
+    ]
+
+    courses = [
+        {"name": name}
+        for name in {assignment['course'] for assignment in new_assignments}
+    ]
+    
+
+    search_request.raise_for_status()
+    
+    for db in search_request.json().get("results", []):
+        name = db.get("title", [{}])[0].get("text", {}).get("content", {})
+        if name == expected_title:
+            id = db.get("id")
+            
+            course_names = [course['name'] for course in courses]
+            for course in db.get("properties", {}).get("Course", {}).get("select", {}).get("options", []):
+                if course['name'] not in course_names:
+                    course_names.append(course['name'])
+                    courses.append({"name": course['name']})
+
+            break
+
+    database_format["properties"]["Course"]["select"]["options"] = courses
+
+    if not id:
+        
+        database_format["parent"] = parent
+        database_format['title'] = [
+            {
+                "type": "text",
+                "text": {
+                    "content": expected_title
+                }
+            }
+        ]
+        
+        created_db_response = r.post(
+            "https://api.notion.com/v1/databases",
+            headers=headers,
+            json=database_format
+        )
+        created_db_response.raise_for_status()
+        id = created_db_response.json().get("id")
+        properties = created_db_response.json()['properties']
+    else:
+        updated_db_response = r.patch(
+            f"https://api.notion.com/v1/databases/{id}",
+            headers=headers,
+            json=database_format
+        )
+        updated_db_response.raise_for_status()
+        properties = updated_db_response.json()['properties']
+
+    return (id, properties, new_assignments, other_assignments)
+        
+def list_db_items(id, key):
+    response = r.post(
+        "https://api.notion.com/v1/databases/{}/query".format(id),
+        headers = {
+            "Authorization": f"Bearer {key}",
+            'Content-Type': 'application/json',
+            'Notion-Version': '2022-06-28'
+        }
+    )
+    
+    response.raise_for_status()
+    
+    return response.json().get("results", [])
+        
+def update_notion(assignments, data):
+    
+    id, base_properties, new_assignments, other_assignments = confirm_notion_database(data, assignments)
+    items = list_db_items(id, data['Notion']['Notion-API-Key'])
+
+    existing_items = [
+        item['properties']["Canvas-Assignment-ID"]["rich_text"][0]["text"]["content"] for item in items
+        if item['properties']["Canvas-Assignment-ID"]["rich_text"]
+    ]
+    
+    for assignment in new_assignments:
+
+        if assignment['id'] in existing_items:
+            continue
+        
+        time = dt.fromisoformat(assignment['due_at'].replace("Z", "+00:00"))
+
+        soup = bs(assignment.get("description", ""), "html.parser")
+        try:
+            text = soup.get_text(separator="\n").strip()
+        except TypeError:
+            text = assignment.get("description", "")
+        text = f"{text[0:1997]}..." if len(text) > 2000 else text
+
+        properties = base_properties.copy()
+        properties.update({
+            "Description": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": text
+                        }
+                    }
+                ]
+            },
+            "Due Date": {
+                "date": {
+                    "start": time.isoformat(timespec='milliseconds'),
+                    "end": None,
+                    "time_zone": None
+                }
+            },
+            "Status": {
+                "select": {
+                    "name": "Graded" if assignment['graded'] else "Submitted" if assignment['submitted'] else "Not started"
+                }
+            },
+            "Course": {
+                "select": {
+                    "name": assignment['course']
+                }
+            },
+            "Canvas-Assignment-ID": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": assignment['id']
+                        }
+                    }
+                ]
+            },
+            "Name": {
+                "title": [
+                    {
+                        "text": {
+                            "content": assignment['name']
+                        }
+                    }
+                ]
+            },
+            "Link": {
+                "url": assignment['url']
+            }
+        })
+        
+        response = r.post(
+            "https://api.notion.com/v1/pages",
+            headers = {
+                'Authorization': f"Bearer {data['Notion']['Notion-API-Key']}",
+                'Content-Type': 'application/json',
+                'Notion-Version': '2022-06-28'
+            },
+            json = {
+                "parent": {
+                    "database_id": id
+                },
+                "properties": properties
+            }
+        )
+        
+        response.raise_for_status()
+
+    assignment_map = {
+        assignment['id']: assignment for assignment in new_assignments
+    }
+
+    for assignment in items:
+        if not assignment['properties']["Canvas-Assignment-ID"]["rich_text"]:
+            continue
+        assignment_details = assignment_map.get(assignment['properties']["Canvas-Assignment-ID"]["rich_text"][0]["text"]["content"])
+        if not assignment_details:
+            continue
+
+        time = dt.fromisoformat(assignment_details['due_at'].replace("Z", "+00:00"))
+        old_status = assignment['properties']["Status"]["select"]["name"]
+
+        if assignment_details['graded']:
+            new_status = "Graded"
+        elif assignment_details['submitted']:
+            new_status = "Submitted"
+        else:
+            new_status = old_status
+
+        r.patch(f"https://api.notion.com/v1/pages/{assignment['id']}",
+            headers = {
+                'Authorization': f"Bearer {data['Notion']['Notion-API-Key']}",
+                'Content-Type': 'application/json',
+                'Notion-Version': '2022-06-28'
+            },
+            json= {
+            "properties": {
+                "Due Date": {
+                    "date": {
+                        "start": time.isoformat(timespec='milliseconds'),
+                        "end": None,
+                        "time_zone": None
+                    }
+                },
+                "Status": {
+                    "select": {
+                        "name": new_status
+                    }
+                },
+                "Link": {
+                    "url": assignment['url']
+                }
+            }
+        })
+
+def scrape_assignments(data):
+    
+    all_assignments = []
+
+    for canvas in data[integration_title]["canvases"]:
+        response = r.get("https://canvas.instructure.com/api/v1/courses", headers={
+            "Authorization": f"Bearer {canvas['canvas-api-token']}",
+        })
+        
+        response.raise_for_status()
+        
+        courses = []
+        
+        for course in response.json():
+            
+            try:
+                if course['course_code'] in canvas["excluded-course-codes"]:
+                    continue
+                
+                courses.append({
+                    "id": course['id'],
+                    "name": course['name'],
+                    "url": f"{canvas['canvas-api-url']}/api/v1/courses/{course['id']}"
+                })
+            except KeyError as e:
+                pass
+        
+        
+        for course in courses:
+            all_assignments.extend(get_assignments(course, canvas['canvas-api-token']))
+            
+    return all_assignments
+
+def check_config(config):
+    with config.lock:
+        data = config.get_data()
+        if integration_title not in data:
+            data[integration_title] = config_defaults
+            config.write_data()
+        else:
+            for key in config_defaults.keys():
+                if key not in data[integration_title]:
+                    data[integration_title][key] = config_defaults[key]
+                    config.write_data()
+
+        forced_update = False
+        for canvas in data[integration_title]["canvases"]:
+            for key in canvas_defaults.keys():
+                if key not in canvas or canvas[key] is None:
+                    forced_update = True
+                    canvas[key] = canvas_defaults[key]
+                    
+        if forced_update:
+            config.write_data()
+            raise Exception("Canvas Configuration is Incomplete")
+
+def main(stop_event, config):
+    try:
+        check_config(config)
+    except Exception as e:
+        print(f"Error in Canvas integration: {e}")
+        stop_event.set()
+        
+    while not stop_event.is_set():
+        
+        with config.lock:
+            data = copy.deepcopy(config.get_data())
+        
+        assignments = scrape_assignments(data)
+        update_notion(assignments, data)
+        break
---
+++ main.py
@@ -0,0 +1,83 @@
+import os
+import threading
+import importlib
+import json
+
+base_notion_config = {
+    "Notion-API-Key": None,
+    "parent-page-id": None
+}
+
+nullable_keys = []
+
+stop_event = threading.Event()
+
+class Config:
+    def __init__(self, filename="config.json"):
+        
+        self.filename = filename
+        self.lock = threading.Lock()
+        
+        try:
+            with open(filename, "r") as f:
+                self._data = json.loads(f.read())
+        except Exception as e:
+            print(e)
+            self._data = {}
+
+    def get_data(self):
+        return self._data
+        
+    def write_data(self):
+        with open(self.filename, "w") as f:
+            f.write(json.dumps(self._data, indent=2))
+
+def check_config(config):
+    with config.lock:
+        data = config.get_data()
+        if "Notion" not in data:
+            data["Notion"] = base_notion_config
+            config.write_data()
+
+        for key in base_notion_config.keys():
+            if key not in data["Notion"]:
+                data["Notion"][key] = base_notion_config[key]
+                config.write_data()
+
+        for key, value in data["Notion"].items():
+            if value is None and key not in nullable_keys:
+                config.write_data()
+                raise Exception("Notion Configuration is Incomplete")
+
+def main():
+    threads = []
+    config = Config()
+    check_config(config)
+    
+    for integration in os.listdir():
+        dirpath = os.path.join(".", integration)
+        if not os.path.isdir(dirpath) or not os.path.exists(os.path.join(dirpath, "main.py")):
+            continue
+        
+        filename = os.path.join(dirpath, "main.py")[2:]
+        module_name = filename.replace(".py", "").replace("/", ".").replace("\\", ".")
+        module = importlib.import_module(module_name)
+        threads.append(threading.Thread(target=module.main, args=(stop_event, config)))
+
+    try:
+        
+        for thread in threads:
+            thread.start()
+            
+        for thread in threads:
+            thread.join()
+            
+    except (KeyboardInterrupt, Exception) as e:
+        stop_event.set()
+            
+        for thread in threads:
+            thread.join()
+
+
+if __name__ == "__main__":
+    main()
